913fd26ea6109c430a7ebda5f10eb7dd6ead2107
Jose Javier Romero Rivas <jjrr1984@gmail.com>
2015-04-08 14:41:29 +0200

    Improving database location

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var app = express();
 
 //Connecting to the database
-mongoose.connect(process.env.DBLOCATION || 'mongodb://localhost/portfolio');
+mongoose.connect('mongodb://' + ( process.env.DBLOCATION || 'localhost/portfolio'));
 var db = mongoose.connection;
 db.on('error', function(){
 	console.log("Database connection error");
