95b8bc4a70ea15ab6575c34d6e43393ea56a5c2d
Jose Javier Romero Rivas <jjrr1984@gmail.com>
2015-04-10 14:00:39 +0200

    Adding comments to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     uglify:{
       options:{
-        mangle: false
+        mangle: false                       // For Angular compatibility
       },
       target:{
         files:[
           {
             expand: true,                   // Enable dynamic expansion.
             cwd: 'angular/',                // Src matches are relative to this path.
-            src: ['**/*.js'],             // Actual pattern(s) to match.
+            src: ['**/*.js'],               // Actual pattern(s) to match.
             dest: 'angular/',               // Destination path prefix.
             ext: '.min.js',                 // Dest filepaths will have this extension.
             extDot: 'first'                 // Extensions in filenames begin after the first dot
