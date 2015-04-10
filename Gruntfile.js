36f211ef23d963890fd8d66680329b9c0f8fea36
Jose Javier Romero Rivas <jjrr1984@gmail.com>
2015-04-10 13:58:26 +0200

    -Removing concat task in Gruntfile plus Node.js module -Adding option "mangle:false" to uglify task (Angular compatibility) -Setting minified files on "index.html" -Adding minified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,16 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    concat:{
-      options:{
-        stripBanners: {
-          options:{
-            block: true,
-            line: true
-          }
-        }
-      },
-      target:{
-        src: ['angular/**/*.js'],
-        dest: 'angular/concat.js'
-      }
-    },
     uglify:{
+      options:{
+        mangle: false
+      },
       target:{
         files:[
           {
             expand: true,                   // Enable dynamic expansion.
             cwd: 'angular/',                // Src matches are relative to this path.
-            src: ['concat.js'],             // Actual pattern(s) to match.
+            src: ['**/*.js'],             // Actual pattern(s) to match.
             dest: 'angular/',               // Destination path prefix.
             ext: '.min.js',                 // Dest filepaths will have this extension.
             extDot: 'first'                 // Extensions in filenames begin after the first dot
@@ -29,15 +18,11 @@ module.exports = function(grunt) {
         ]
       }
     },
-    clean:{
-      min:["angular/**/*.min.js"],
-      concat:["angular/concat.js"]
-    }
+    clean: ["angular/**/*.min.js"]
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('min',['clean:min','concat','uglify','clean:concat']);
+  grunt.registerTask('default',['clean','uglify']);
 };
