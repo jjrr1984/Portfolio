09cac5ce9ea62ad8da2dfabec4c6fabc640c255f
Jose Javier Romero Rivas <jjrr1984@gmail.com>
2015-04-14 11:50:32 +0200

    -Adding cssmin to Gruntfile -Adding cssmin as a new node package -Added the new minified css -Pointing the index.html to the minified css

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,21 @@ module.exports = function(grunt) {
         ]
       }
     },
-    clean: ["angular/all.min.js"]
+    cssmin:{
+      target:{
+        files:[
+          {
+            'css/style.min.css': ['css/style.css']
+          }
+        ]
+      }
+    },
+    clean: ["angular/all.min.js","css/style.min.css"]
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default',['clean','uglify']);
+  grunt.registerTask('default',['clean','uglify','cssmin']);
 };
